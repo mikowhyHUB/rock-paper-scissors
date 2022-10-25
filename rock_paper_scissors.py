cfdbea151aa55b74f59fda41ce6912dd0e0597bb
itsnotmikowhy <111461945+itsnotmikowhy@users.noreply.github.com>
2022-10-25 18:07:33 +0200

    new program

---
+++ rock_paper_scissors.py
@@ -0,0 +1,13 @@
+'''
+plan na apke:
+1. stworzyc podstawowe opcje
+2. dodac bajery
+3. zrobić jak najbardziej profesjonalna
+'''
+# stwórz dwie zmienne liczące kto ile ma punktów i zmienną z moliwymi wyborami
+# uytkownika imput zabezpiecz pętlą while true, mozesz dodac do inputa q czyli quit
+# zabezpiecz input od czgoś innego niz to co ma wpisac
+# komputer  wybiera jedną z trzech opcji(randint) i zmienna wczesnirj stworzona
+# wydrukuj co komputer wygral
+# opracowanie ifelse kto zdobył punkt
+# drukowanie wyników(poza petla while)
