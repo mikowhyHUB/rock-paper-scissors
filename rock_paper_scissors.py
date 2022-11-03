fbedc65d623a21528f75bee87a1363c255cc272f
itsnotmikowhy <111461945+itsnotmikowhy@users.noreply.github.com>
2022-11-03 18:52:22 +0100

    more adv ver

--- rock_paper_scissors.py
+++ rock_paper_scissors.py
@@ -5,40 +5,64 @@ plan na apke:
 2. dodac bajery
 3. zrobić jak najbardziej profesjonalna
 '''
-# make variables for counting and what are users options
+# variables for counting and what are users options
 user_count = 0
 comp_count = 0
-options = ['rock', 'paper', 'scissors']
-print('\nWelcome to the rock, paper, scissors game!\nThe Rules are:\n1. Pick one of three options\n2. After your pick, you will get the results.\n3. If you want to quit and see scores, type "q"')
 
-# adding while true for mulitple options
-while True:
-    user_choice = input('Rock/Paper/Scissors or Q for quit: ').lower()
-    # making quit option and protect program from wrong input
-    if user_choice == 'q':
-        break
-    if user_choice not in options:  # do zabezpieczenia przed errorami
-        print('Wrong pick. Only rock, paper, scissors accepted.')
-        continue
+
+def user_selection(options):
+    while True:  # while true for multiply selection
+        user_choice = input('Rock/Paper/Scissors or Q for quit: ').lower()
+        # making quit option and protect program from wrong input
+        if user_choice == 'q':
+            break
+        if user_choice not in options:  # do zabezpieczenia przed errorami
+            print('Wrong pick. Only rock, paper, scissors accepted.')
+            continue
+        return user_choice
+
+
+def computer_selection(options):
     # using randint for computer choice
     comp_choice = choice(options)
-    print('Your pick:', user_choice)
+    print('Your pick:', user_selection(options))
     print('Computer pick:', comp_choice)
+    return comp_choice
+
+
+def play():
     # counting won games
-    if user_choice == comp_choice:
+    if user_selection() == computer_selection():
         print('tie')
-    elif user_choice == 'rock' and comp_choice == 'scissors':
+    elif user_selection() == 'rock' and computer_selection() == 'scissors':
         print('You won!')
         user_count += 1
-    elif user_choice == 'paper' and comp_choice == 'rock':
+    elif user_selection() == 'paper' and computer_selection() == 'rock':
         print('You won!')
         user_count += 1
-    elif user_choice == 'scisorrs' and comp_choice == 'paper':
+    elif user_selection() == 'scisorrs' and computer_selection() == 'paper':
         print('You won!')
         user_count += 1
     else:
         print('You lose')
         comp_count += 1
+
+
 # printing what scores are
 print(f'You won {user_count} times. Computer won {comp_count} times.')
 # dodać printa z ifelse np good luck next time
+
+
+def main():
+    print('''Welcome to the rock, paper, scissors game!
+    The Rules are:
+    1. Pick one of three options
+    2. After your pick, you will get the results.
+    3. If you want to quit and see scores, type "q"''')
+    options_list = ['rock', 'paper', 'scissors']
+    player_result = user_selection(options_list)
+    computer_result = computer_selection(options_list)
+    # play()
+
+
+main()
