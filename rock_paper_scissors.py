c9c8dd30bf448569a450ad7945168003eb63fb51
itsnotmikowhy <111461945+itsnotmikowhy@users.noreply.github.com>
2022-11-12 10:29:36 +0100

    oop pogram

--- rock_paper_scissors.py
+++ rock_paper_scissors.py
@@ -1,24 +1,5 @@
 from random import randint
 
-'''
-Class ver:
-1.  stowrzyć klasę player, konstruktor init, w nim dwie zmienne instance(nie parametry do konstruktora) choice none , score 0. 
-2. stworzyć metodę get_input z parametrem choice, i zabezpiecz ją od złych enterów:
-- stwórz for loopa by zlupowal options i ifa by sprawidzc czy zgadza się z options. jeeli się zagadza, dodaj do zmiennje 1(1 oznacza, ze jest ok)
-- if poza petla mówiący, ze jak będzie 0 to wyprintować info. Zmienic tez konstruktor run na false. zwrócic run. 
--drugi if jezeli jest 1 to print ze wszystko ok i przyznac choice do choice i zwrocic run(ktory jest noramlnei true)
-3. stworz metode by  zwracala wybor i niech zwraca choice
-4. stworz metode sprawdajaca o parametrze wyboru komputera i na razie pass
-5. stworz metode zwracajaca wynik i niech zwraca wynik
-6. stworz obiekt run który jest standardowo true oraz while loop z runem. 
-- stworz zmienna run i tam uzyj clasy i metody get input. w jako parametry daj input i run
-7. zdefiniuj obiekt przed run = true. inaczej nie bedzie 6 dzialac
-8. stworz if i jezeli run bedzie true to wtedy robimy komputer choice z options i randint
-- wydrukuj wybor komputera
-- stworz kto wygral - klasa. wybór(wybór komputera)
-- poza petla print by zwrocilo wynik
-9. dodaj do metody sprawdzania petle if ktora mowi ze gdy wybor jest inny niz ten podany jako zmienna choice, wtedy nastepna petla i podajemy warunki wygranej i dodajemy punkt do self.score
-'''
 options = ['rock', 'paper', 'scissors']
 
 
